0c59dda72b41b3d9f5e55d61174bc77a896c6e7c
MarcosArauj <marcos.araujocruz@hotmail.com>
2020-07-09 16:48:22 -0300

    Inclusão do Templete AdminLTE3

--- source/Config/Minify.php
+++ source/Config/Minify.php
@@ -1,6 +1,8 @@
 <?php
 
 $minCSS = new \MatthiasMullie\Minify\CSS();
+$minCSS->add(dirname(__DIR__,2).'/views/assets/AdminLTE/dist/css/adminlte.min.css');
+$minCSS->add(dirname(__DIR__,2).'/views/assets/bootstrap4/dist/css/bootstrap.min.css');
 $minCSS->add(dirname(__DIR__,2).'/views/assets/css/load.css');
 $minCSS->add(dirname(__DIR__,2).'/views/assets/css/message.css');
 $minCSS->add(dirname(__DIR__,2).'/views/assets/css/style.css');
@@ -13,5 +15,9 @@ $minJS->add(dirname(__DIR__,2)."/views/assets/js/jquery-ui.js");
 $minJS->add(dirname(__DIR__,2).'/views/assets/js/funcoesUteis.js');
 $minJS->add(dirname(__DIR__,2).'/views/assets/js/validacao.js');
 $minJS->add(dirname(__DIR__,2).'/views/assets/js/mascara.js');
+$minJS->add(dirname(__DIR__,2).'/views/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js');
+$minJS->add(dirname(__DIR__,2).'/views/assets/AdminLTE/dist/js/adminlte.min.js');
+$minJS->add(dirname(__DIR__,2).'/views/assets/AdminLTE/dist/js/demo.js');
 $minJS->minify(dirname(__DIR__,2).'/views/assets/js/scripts.min.js');
+
 
