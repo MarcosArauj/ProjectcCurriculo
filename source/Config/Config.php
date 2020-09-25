b4e84c5107b8258ad2c8bff43ded6145f6be454f
MarcosArauj <marcos.araujocruz@hotmail.com>
2020-09-25 20:00:51 -0300

    Função para Limpar Cache

--- source/Config/Config.php
+++ source/Config/Config.php
@@ -8,8 +8,8 @@ define("SITE", [
     "desc" => "Sistema Web Controle de  Curriculo",
     "domain" => "curriculotcc.com.br",
     "locale" => "pt_BR",
-    "root" => "http://curriculotcc.com.br",
-    //"root" => "http://wcurriculo.cruzm.com.br",
+   // "root" => "http://curriculotcc.com.br",
+    "root" => "http://wcurriculo.cruzm.com.br",
     "version"=>"1.0"
 ]);
 
@@ -17,7 +17,7 @@ define("SITE", [
  *  SITE MINIFY
  */
 
-if($_SERVER["SERVER_NAME"] == "localhost") {
+if($_SERVER["SERVER_NAME"] == "185.201.11.23") {
     require __DIR__ . "/Minify.php";
 }
 
