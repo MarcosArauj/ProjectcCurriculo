5eb7443dc0ba45da63bdf43e0361c905fe5cdd1c
MarcosArauj <marcos.araujocruz@hotmail.com>
2020-09-25 20:45:10 -0300

    Função para Limpar Cache

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 ob_start();
 
-clearstatcache();
 
-header("Pragma: no-cache");
-header("Cache: no-cache");
-header("Cache-Control: no-cache, must-revalidate");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 25 Jan 2020 05:00:00 GMT"); // Date in the past
 // Rotas
 require __DIR__.'/source/routes.php';
